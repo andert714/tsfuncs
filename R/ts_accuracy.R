91993c9904b21a1e3073118c95ef8a1dbaeaf003
Travis Andersen <travisandersen14@gmail.com>
2020-05-15 15:21:19 -0600

    Install package

---
+++ R/ts_accuracy.R
@@ -0,0 +1,34 @@
+#' Time series accuracy
+#'
+#' @param ts_df A tibble containing dates in column labeled 'ds' and values in column labeled 'y'.
+#' The point forecast should be in a column labeled 'yhat', and respective prediction intervals should
+#' be in columns labeled 'yhat_lower' and 'yhat_upper'.
+#'
+#' @param lag The lag to be used in calculating MASE, which should be the period of seasonality of the time series.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' df <- tidyr::tibble(
+#'   ds = lubridate::today() - 100:1,
+#'   y = rnorm(100),
+#'   yhat = rnorm(100),
+#'   yhat_lower = yhat - 1,
+#'   yhat_upper = yhat + 1
+#' )
+#' ts_accuracy(df)
+#' #' @importFrom dplyr %>%
+ts_accuracy <- function(ts_df, lag = 1){
+  ts_df %>%
+    dplyr::summarize(
+      ME = mean(y - yhat, na.rm = TRUE),
+      MAE = mean(abs(y - yhat), na.rm = TRUE),
+      MASE = mean(abs(y - yhat), na.rm = TRUE)/mean(abs(y - dplyr::lag(y, lag)), na.rm = TRUE),
+      Coverage = mean(yhat_lower <= y & yhat_upper >= y, na.rm = TRUE),
+      Width = mean(yhat_upper - yhat_lower, na.rm = TRUE)
+    ) %>%
+    dplyr::arrange(MASE, dplyr::desc(Coverage))
+}
+
+
--- R/ts_graph.R
+++ R/ts_graph.R
@@ -1,12 +1,27 @@
-ts_graph <- function(ts){
-  graph <- ts %>%
-    select('y', contains('yhat')) %>%
-    xts::xts(ts$ds) %>%
+#' Graph time series
+#'
+#' Create an interactive graph of a time series tibble.
+#'
+#' @param ts_df A tibble containing dates in column labeled 'ds' and
+#' values in column labeled 'y'.
+#' If forecast is contained, the point forecast should be in a column labeled 'yhat', and
+#' respective prediction intervals should be in columns labeled 'yhat_lower' and 'yhat_upper'.
+#'
+#' @return An interactive graph.
+#' @export
+#'
+#' @examples
+#' ts_graph(tidyr::tibble(ds = lubridate::today() - 100:1, y = rnorm(100)))
+#' @importFrom dplyr %>%
+ts_graph <- function(ts_df){
+  graph <- ts_df %>%
+    dplyr::select('y', dplyr::contains('yhat')) %>%
+    xts::xts(ts_df$ds) %>%
     dygraphs::dygraph() %>%
     dygraphs::dySeries('y', 'Actual', 'black') %>%
     dygraphs::dyRangeSelector()
 
-  if('yhat' %in% names(ts)){
+  if('yhat' %in% names(ts_df)){
     graph <- graph %>%
       dygraphs::dySeries(c('yhat_lower', 'yhat', 'yhat_upper'), 'Forecast', 'blue')
   }
